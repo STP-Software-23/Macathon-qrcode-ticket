1a2a95a32b29227089d49b85546cd5a7dac04463
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-02 02:24:31 +0200

    feat: Add QR code generator function

--- qrcodeGenerator.php
+++ qrcodeGenerator.php
@@ -1,5 +1,22 @@
 <?php 
-require_once('phpqrcode/qrlib.php');
+require_once('./lib/phpqrcode/qrlib.php');
+
+// $data = [
+//     "name" => "test",
+//     "age" => 17
+// ];
+
+function generateQR($data , $dataID){
+    $text = json_encode($data, JSON_PRETTY_PRINT);
+    $path = 'images/';
+    $filename = 'QRcodes/qrcode'.$dataID.'.png';
+    $level = QR_ECLEVEL_M;
+    $size = 5;
+    $margin = 4;
+    QRcode::png($text, $filename, $level, $size, $margin);
+}
+
+// generateQR($data,2)
 
 
 ?>
